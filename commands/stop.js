101be2e695b788619ef27eb0e26ffc1dce242d90
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 21:02:46 +0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,16 +2,16 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 
 module.exports = {
-  name: "stop",
-  description: "Stops the music",
+  name: "durdur",
+  description: "Şarkıyı durdurur",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("**__Şuanda hiçbir şarkı çalmıyor__**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.songs = [];
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏹ **__şarkıyı durdurdu!__**`).catch(console.error);
   }
 };
