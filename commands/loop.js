2b1edc3238f6e55150110b3e5c8271bd79e01251
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:25:52 +0300

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,18 +1,18 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
-  name: "loop",
+  name: "döngü",
   aliases: ['l'],
   description: "Toggle music loop",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply(":no_entry_sign:  **__Şuanda şarkı çalmıyor.**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     // toggle from false to true and reverse
     queue.loop = !queue.loop;
     return queue.textChannel
-      .send(`Loop is now ${queue.loop ? "**on**" : "**off**"}`)
+      .send(`**__Şarkı döngüsü :** ${queue.loop ? "**açık**" : "**kapalı**"}`)
       .catch(console.error);
   }
 };
