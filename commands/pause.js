c6570ee5c0207f846af2000d2856bafa9d4852ec
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:28:34 +0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,17 +1,17 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
-  name: "pause",
+  name: "durdur",
   description: "Pause the currently playing music",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply(":no_entry_sign: **__Şuanda hiçbir şarkı çalmıyor.**__").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
       queue.playing = false;
       queue.connection.dispatcher.pause(true);
-      return queue.textChannel.send(`${message.author} ⏸ paused the music.`).catch(console.error);
+      return queue.textChannel.send(`${message.author} ⏸ **şarkıyı durdurdu.**`).catch(console.error);
     }
   }
 };
