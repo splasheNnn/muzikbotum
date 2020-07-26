ee04d93dae847f05e5fb0bb7f62be9746c8e4a20
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:55:40 +0300

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,11 +1,11 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
-  name: "shuffle",
-  description: "Shuffle queue",
+  name: "karıştır",
+  description: "Şarkı sırasını karışık çalacak şekilde ayarlar",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue) return message.channel.send(":no_entry_sign: **__Şu anda bir sıra yok.__**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     let songs = queue.songs;
@@ -15,6 +15,6 @@ module.exports = {
     }
     queue.songs = songs;
     message.client.queue.set(message.guild.id, queue);
-    queue.textChannel.send(`${message.author} 🔀 shuffled the queue`).catch(console.error);
+    queue.textChannel.send(`${message.author} 🔀 **__Şarkı sırasını karıştırdı__**`).catch(console.error);
   }
 };
