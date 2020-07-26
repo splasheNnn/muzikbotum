2bf334306da26d0390cc5b8fa2c85501f9316ab2
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:56:23 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,17 +1,17 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
-  name: "skip",
-  aliases: ["s"],
-  description: "Skip the currently playing song",
+  name: "geç",
+  aliases: ["g"],
+  description: "Çalmakta olan şarkıyı geçer",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
-      return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+      return message.reply("**__Şu anda hiçbir şarkı çalmıyor__**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏭ **__şarkıyı geçti__**`).catch(console.error);
   }
 };
