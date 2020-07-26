48a145ff9a5fcbb87b1cc6ab96949d69528e0629
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:42:25 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "help",
+  name: "yardım",
   aliases: ["h"],
   description: "Display all commands and descriptions",
   execute(message) {
