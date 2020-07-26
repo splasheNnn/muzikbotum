885b9ad12085ec2c4cd0f48e26fe724148474e9b
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:40:16 +0300

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -15,19 +15,19 @@ module.exports = {
 
     const serverQueue = message.client.queue.get(message.guild.id);
     if (serverQueue && channel !== message.guild.me.voice.channel)
-      return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error);
+      return message.reply(`:no_entry_sign:  ${message.client.user} **__İle aynı kanalda olmalısın__**`).catch(console.error);
 
     if (!args.length)
       return message
-        .reply(`Usage: ${message.client.prefix}playlist <YouTube Playlist URL | Playlist Name>`)
+        .reply(`Usage: ${message.client.prefix}playlist <YouTube Çalma Listesi URL | Çalma Listesi İsmi>`)
         .catch(console.error);
-    if (!channel) return message.reply("You need to join a voice channel first!").catch(console.error);
+    if (!channel) return message.reply(":no_entry_sign: **__Önce bir sesli kanala giriş yapmalısın__**").catch(console.error);
 
     const permissions = channel.permissionsFor(message.client.user);
     if (!permissions.has("CONNECT"))
-      return message.reply("Cannot connect to voice channel, missing permissions");
+      return message.reply(":no_entry_sign: **__İstenen odaya giriş için yetkim yok__**");
     if (!permissions.has("SPEAK"))
-      return message.reply("I cannot speak in this voice channel, make sure I have the proper permissions!");
+      return message.reply(":no_entry_sign: **__Odada konuşmak için yetkim yok!**");
 
     const search = args.join(" ");
     const pattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/gi;
@@ -63,7 +63,7 @@ module.exports = {
         videos = await playlist.getVideos(MAX_PLAYLIST_SIZE || 10, { part: "snippet" });
       } catch (error) {
         console.error(error);
-        return message.reply("Playlist not found :(").catch(console.error);
+        return message.reply(":no_entry_sign: **__Çalma Listesi Bulunamadı__** :(").catch(console.error);
       }
     }
 
@@ -78,7 +78,7 @@ module.exports = {
         serverQueue.songs.push(song);
         if (!PRUNING)
           message.channel
-            .send(`✅ **${song.title}** has been added to the queue by ${message.author}`)
+            .send(`✅ **${song.title}** **__başarıyla__** ${message.author} __**tarafından sıraya eklendi**__`)
             .catch(console.error);
       } else {
         queueConstruct.songs.push(song);
@@ -95,10 +95,10 @@ module.exports = {
       playlistEmbed.setDescription(queueConstruct.songs.map((song, index) => `${index + 1}. ${song.title}`));
       if (playlistEmbed.description.length >= 2048)
         playlistEmbed.description =
-          playlistEmbed.description.substr(0, 2007) + "\nPlaylist larger than character limit...";
+          playlistEmbed.description.substr(0, 2007) + "\nÇalmaListesi karakter limitinin üzerinde";
     }
 
-    message.channel.send(`${message.author} Started a playlist`, playlistEmbed);
+    message.channel.send(`${message.author} **__Çalma listesi çalınıyor__**`, playlistEmbed);
 
     if (!serverQueue) message.client.queue.set(message.guild.id, queueConstruct);
 
@@ -111,7 +111,7 @@ module.exports = {
         console.error(error);
         message.client.queue.delete(message.guild.id);
         await channel.leave();
-        return message.channel.send(`Could not join the channel: ${error}`).catch(console.error);
+        return message.channel.send(`:no_entry_sign: **__İstenen odaya giriş için yetkim yok:__** ${error}`).catch(console.error);
       }
     }
   }
