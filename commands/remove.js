862a56e076c5dd2e20274d71cd7538e320582e03
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:51:34 +0300

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,17 +1,17 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
-  name: "remove",
-  description: "Remove song from the queue",
+  name: "sil",
+  description: "Sıradan müzik siler",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue) return message.channel.send(":no_entry_sign: **__Şuanda sıra yok__**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
     
-    if (!args.length) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
+    if (!args.length) return message.reply(`**Kullanım:** ${message.client.prefix}sil <Şarkının Sıra Numarası>`);
+    if (isNaN(args[0])) return message.reply(`**Kullanım:** ${message.client.prefix}sil <Şarkının Sıra Numarası>`);
 
     const song = queue.songs.splice(args[0] - 1, 1);
-    queue.textChannel.send(`${message.author} ❌ removed **${song[0].title}** from the queue.`);
+    queue.textChannel.send(`${message.author} ❌  **${song[0].title}** **__adlı şarkıyı sıradan sildi.__**`);
   }
 };
