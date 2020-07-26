30ed407743ba57dddb348c402db8280a1725d98f
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:54:50 +0300

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -4,21 +4,21 @@ const YouTubeAPI = require("simple-youtube-api");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 
 module.exports = {
-  name: "search",
-  description: "Search and select videos to play",
+  name: "ara",
+  description: "Youtubeden şarkı veya video aratıp çalmak için seçersiniz",
   async execute(message, args) {
     if (!args.length)
-      return message.reply(`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`).catch(console.error);
+      return message.reply(`**Kullanım:** ${message.client.prefix}${module.exports.name} <Video İsmi>`).catch(console.error);
     if (message.channel.activeCollector)
-      return message.reply("A message collector is already active in this channel.");
+      return message.reply("");
     if (!message.member.voice.channel)
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+      return message.reply(":no_entry_sign: **__Önce bir sesli kanala girmelisin__**").catch(console.error);
 
     const search = args.join(" ");
 
     let resultsEmbed = new MessageEmbed()
-      .setTitle(`**Reply with the song number you want to play**`)
-      .setDescription(`Results for: ${search}`)
+      .setTitle(`**Arattığınız isimde çıkan sonuçlar**`)
+      .setDescription(`Sonuçlar : ${search}`)
       .setColor("#F8AA2A");
 
     try {
