3975d5095422ec5f0383fe6ab1734ccb350c35d0
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 21:06:38 +0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Çalan şarkıyı durdurur",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply(":no_entry_sign: **__Şuanda hiçbir şarkı çalmıyor.**__").catch(console.error);
+    if (!queue) return message.reply(":no_entry_sign: **__Şuanda hiçbir şarkı çalmıyor.__**").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
