b275e64077ddfd9938d4f76ff78fefb4d4b2e46f
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 21:16:23 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `**${message.client.prefix}${cmd.name} `()` : ""}**`,
         `${cmd.description}`,
         true
       );
