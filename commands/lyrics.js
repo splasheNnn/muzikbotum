6e5cc8f9eb57bd33facf0b7d6f6b12a0f196eaad
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-26 20:27:29 +0300

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -2,18 +2,18 @@ const { MessageEmbed } = require("discord.js");
 const lyricsFinder = require("lyrics-finder");
 
 module.exports = {
-  name: "lyrics",
+  name: "sözler",
   aliases: ["ly"],
   description: "Get lyrics for the currently playing song",
   async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    if (!queue) return message.channel.send(":no_entry_sign: **__Şuanda şarkı çalmıyor.__**").catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      if (!lyrics) lyrics = `:no_entry_sign:  __**Bu şarkının sözleri bulunamadı** ${queue.songs[0].title}.`;
     } catch (error) {
       lyrics = `No lyrics found for ${queue.songs[0].title}.`;
     }
