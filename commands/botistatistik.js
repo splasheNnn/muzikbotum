608c523b9705d6fdb98b371ce3576ae08a705739
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-27 16:25:27 +0300

    Update botistatistik.js

--- commands/botistatistik.js
+++ commands/botistatistik.js
@@ -1,43 +1,45 @@
 const Discord = require("discord.js");
 const moment = require("moment");
-const os = require("os");
+const os = require('os');
 require("moment-duration-format");
-exports.run = async (client, message, args) => {
-  const seksizaman = moment
-    .duration(client.uptime)
-    .format(" D [gün], H [saat], m [dakika], s [saniye]");
-  const istatistikler = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTimestamp()
-    .setFooter("© 2020 ro-BOT", client.user.avatarURL())
-    .addField("» **Botun Sahibi**", "<@idniz>")
-    .addField("» **Gecikme süreleri**","Mesaj Gecikmesi: {ping1} ms \nBot Gecikmesi: {ping2} ms"
-        .replace("{ping1}", new Date().getTime() - message.createdTimestamp)
-        .replace("{ping2}", client.ws.ping),true)
-    .addField("» **Bellek kullanımı**",(process.memoryUsage().heapUsed / 1024 / 512).toFixed(2) + " MB",true)
-    .addField("» **Çalışma süresi**", seksizaman, true)
-    .addField("» **Kullanıcılar**",client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
-    .addField("» **Sunucular**", client.guilds.cache.size.toLocaleString(), true)
-    .addField("» **Kanallar**", client.channels.cache.size.toLocaleString(), true)
-    .addField("» **Discord.JS sürüm**", "v" + Discord.version, true)
-    .addField("» **Node.JS sürüm**", `${process.version}`, true)
-    .addField("» **Müzik Çalınan Sunucu Sayısı**", client.voice.connections.size, true)
-    .addField("» **CPU**",`\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``,true)
-    .addField("» **Bit**", `\`${os.arch()}\``, true)
-    .addField("» **İşletim Sistemi**", `\`\`${os.platform()}\`\``, true)
-    .addField("**» Bot Davet**"," [Davet Et](https://discordapp.com/oauth2/authorize?client_id=" + client.user.id + "&scope=bot&permissions=8)");
-  return message.channel.send(istatistikler);
-};
-
+exports.run = async (bot, message, args) => {
+  const duration = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+ 
+  let msg = message
+   const bunemq = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const annencilermaldır = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setFooter('GuardMayFe  \'Buyur benim istatistiklerim', bot.user.avatarUR())
+  .addField("» **Botun Sahibi**", "<402047297963294730>| ByMayFe_0#2956  ")
+  .addField("»  **Geliştirici** ","<373493092881530887>| seyfttnbdk_0#3548 ")
+  .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 512 / 512).toFixed(2) + ' MB', true)  
+  .addField("» **Çalışma süresi**", bunemq)
+  .addField('» **Müzik Çalınan Sunucu Sayısı**;', bot.voiceConnections.cache.size)
+  .addField('» **Kullanıcılar**:', bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0))
+  .addField("» **Sunucular**", bot.guilds.cache.size.toLocaleString(), true)
+  .addField("» **Kanallar**", bot.channels.cache.size.toLocaleString(), true)
+  .addField("» **Discord.JS sürüm**", "v"+Discord.version, true)
+  .addField("» **Node.JS sürüm**", `${process.version}`, true)
+  .addField("» **Ping**", bot.ping+" ms", true)
+  .addField("» **CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+  .addField("» **Bit**", `\`${os.arch()}\``, true)
+  .addField("» **İşletim Sistemi**", `\`\`${os.platform()}\`\``)
+  .addField("**» Bot Davet**", " [Davet Et](https://discordapp.com/oauth2/authorize?client_id=597419749995053056&scope=bot&permissions=2146958847)", )
+  .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](https://discord.gg/fqdsZPd)", )
+  .addField("**» Voteleme sayfası**", " [OYLAR MISIN?](https://top.gg/bot/596071936799277116/vote)", )
+   //mayfeBot
+ return message.channel.send(annencilermaldır);
+  };
+ 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["i"],
+  aliases: [ 'i'],
   permLevel: 0
 };
-
+ 
 exports.help = {
   name: "istatistik",
-  description: "Botun istatistiklerini gösterir",
+  description: "Bot i",
   usage: "istatistik"
 };
