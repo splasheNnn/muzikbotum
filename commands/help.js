14a85fd3a27bf96025a28341404acdc137d53ce2
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-28 20:19:11 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,17 +5,7 @@ module.exports = {
   aliases: ["y"],
   description: "Müzik komutlarını listeler",
   execute(message) {
-    let commands = message.client.commands.array();
-
-	   let exampleEmbed = new MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Botu Sunucunuza Davet edin')
-	.setURL('https://discord.com/oauth2/authorize?client_id=672006644128940032&scope=bot&permissions=2146958847')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-	  
+    let commands = message.client.commands.array() 
     let helpEmbed = new MessageEmbed()
       .setTitle("Izuku Müzik | Yardım")
       .setImage("https://media2.giphy.com/media/kggtkeYWsl9aLpH5to/giphy.gif")
@@ -36,7 +26,6 @@ module.exports = {
     helpEmbed.setTimestamp();
 
     return message.channel.send(helpEmbed).catch(console.error);
-	  return message.channel.send(exampleEmbed);
     
     
 
