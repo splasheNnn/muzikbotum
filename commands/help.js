cc7823794b6b3ef9f9e8059620d5c3b687187097
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-28 19:50:52 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,15 @@ module.exports = {
   execute(message) {
     let commands = message.client.commands.array();
 
+	   let exampleEmbed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Botu Sunucunuza Davet edin')
+	.setURL('https://discord.com/oauth2/authorize?client_id=672006644128940032&scope=bot&permissions=2146958847')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	  
     let helpEmbed = new MessageEmbed()
       .setTitle("Izuku Müzik | Yardım")
       .setImage("https://media2.giphy.com/media/kggtkeYWsl9aLpH5to/giphy.gif")
@@ -27,17 +36,11 @@ module.exports = {
     helpEmbed.setTimestamp();
 
     return message.channel.send(helpEmbed).catch(console.error);
+	  return message.channel.send(exampleEmbed);
     
     
- let exampleEmbed = new MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Botu Sunucunuza Davet edin')
-	.setURL('https://discord.com/oauth2/authorize?client_id=672006644128940032&scope=bot&permissions=2146958847')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-channel.send(exampleEmbed);
+
+
   }
 };
