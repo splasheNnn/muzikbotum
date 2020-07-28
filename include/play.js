5de0728ec99460bc86a45051cc95fef0753a1211
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-28 18:25:06 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -84,6 +84,10 @@ module.exports = {
           queue.playing = true;
           reaction.users.remove(user).catch(console.error);
           if (!canModifyQueue(member)) return;
+          if(!message.member.hasPermissions("BAN_MEMBERS")) return message.channel.send(
+    new Discord.RichEmbed() 
+.setColor('RED')
+.setDescription(':x_:  | Bu komutu kullanamasın çünkü **BAN_MEMBERS** yetkisine sahip değilsin.')).then(message => message.delete(5000)); 
           queue.connection.dispatcher.end();
           queue.textChannel.send(`${user} ⏩ **şarkıyı geçti**`).catch(console.error);
           collector.stop();
