5fb487b24337261f08d69a97a082e987ae44a4cc
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-28 21:14:46 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -70,7 +70,7 @@ module.exports = {
       console.error(error);
     }
 
-   const filter = (reaction, user) => user.id !== message.client.user.id && user.hasPermission("BAN_MEMBERS");
+   const filter = (reaction, member) => member.id !== message.client.user.id && member.hasPermission("BAN_MEMBERS");
     var collector = playingMessage.createReactionCollector(filter, {
       time: song.duration > 0 ? song.duration * 1000 : 600000
     });
