e8f588b853f6a6c1a43f26cedd2a5597ee091178
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-29 19:18:56 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,35 +1,38 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 
-module.exports = {
-  name: "avatar",
-  aliases: ["y"],
-  description: "Müzik komutlarını listeler",
-  execute(message) {
-    let commands = message.client.commands.array() 
-    let user = message.mentions.users.first()
-    let helpEmbed = new MessageEmbed() 
-      .setTitle("Kullanıcının avatarı!")
-      .setImage(user.displayAvatarURL())
-      .setColor("#F8AA2A");
-      
-      
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**✯ |  ${message.client.prefix}${cmd.name} **`,
-        `${cmd.description}`,
-        
+exports.run = async (client, message, args) => {
+  let user;
   
-        true
-      );
-    });
-
-    helpEmbed.setTimestamp();
-
-    return message.channel.send(helpEmbed).catch(console.error);
-    
-    
-
-
-
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+  } else if (args[0]) {
+    user = message.guild.members.cache.get(args[0]).user;
+  } else {
+    user = message.author;
   }
-};
+  
+  let avatar = user.displayAvatarURL({size: 4096, dynamic: true});
+  // 4096 is the new biggest size of the avatar.
+  // Enabling the dynamic, when the user avatar was animated/GIF, it will result as a GIF format.
+  // If it's not animated, it will result as a normal image format.
+  
+  const embed = new Discord.MessageEmbed()
+  .setTitle(`${user.tag} avatar`)
+  .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
+  .setColor(0x1d1d1d)
+  .setImage(avatar)
+  
+  return message.channel.send(embed);
+}
+
+exports.help = {
+  name: "avatar",
+  description: "Display a user avatar",
+  usage: "avatar [@user | user ID]",
+  example: "avatar @ray#1337 \navatar"
+}
+
+exports.conf = {
+  aliases: ["icon", "pfp", "ava"],
+  cooldown: 5
+}
