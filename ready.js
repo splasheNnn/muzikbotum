acea4707afbf98a40ede0803423da1fbbba8906e
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-30 00:50:54 +0300

    Create ready.js

---
+++ ready.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+
+module.exports = client => {
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("idle");
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`                                                                                                                                                                     `)
+  client.user.setActivity(` 👑 ▏Prison Break `, { type: "WATCHING"});
+  //LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
+  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
