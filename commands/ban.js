fb246788c4bec9be48e4221694b866cb6ca2a1b1
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-30 19:54:58 +0300

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "ban",
+  description: "Etiketlenen kişiyi sunucudan banlar",
+  execute(message) {
+
+ if (!message.guild) return;
+
+ 
+  if (message.content.startsWith('!!ban')) {
+    
+    const user = message.mentions.users.first();
+  
+    if (user) {
+   
+      const member = message.guild.member(user);
+    
+      if (member) {
+      
+        member
+          .ban('Bir sorun meydana geldi')
+          .then(() => {
+         
+            message.reply(`Başarıyla atıldı ${user.tag}`);
+          })
+          .catch(err => {
+           
+            message.reply('Bir sorun meydana geldi');
+         
+            console.error(err);
+          });
+      } else {
+      
+        message.reply("Kişi sunucuda bulunmuyor!");
+      }
+    
+    } else {
+      message.reply("Kicklemem için herhangi birisini etiketlemedin!");
+    }
+  }
+}
+
+  }
+;
