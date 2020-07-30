e909f15c567a63efa0420ad8dda92584a432b771
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-30 12:33:33 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,18 +20,7 @@ module.exports = {
         
         `______________________________________________________`
         
-        `
-  <a:tik3:735917623551852575> ▏** Botu Sunucuna Davet Et =** [Davet](https://discord.com/oauth2/authorize?client_id=656531150897938453&scope=bot&permissions=2146958591)
-
-
-  <a:tik3:735917623551852575> ▏** Destek Sunucusu =** [Destek](https://discord.gg/VMCb3yR)
-   
-
-  <a:tik3:735917623551852575> ▏** Atha Bot Sitesi =** [Site](https://atha-bot-site.glitch.me/)
-
-
-  <a:tik3:735917623551852575> ▏** Bota Oy Ver =** [Oy](https://top.gg)`
-        
+ 
   
         true
       );
