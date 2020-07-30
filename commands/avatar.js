9fa3f38a25ba1565631846a86742d0dde95181a9
splasheNnn <68560948+splasheNnn@users.noreply.github.com>
2020-07-30 14:04:22 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
 
     let nowPlaying = new MessageEmbed()
       .setTitle("**İşte Avatarın**")
-      .setDescription(`${message.author.avatarURL()}`)
+      .setImage( `${msg.author.avatarURL({size : 1024})}`)
       .setColor("#F8AA2A")
       .setTimestamp();
 
